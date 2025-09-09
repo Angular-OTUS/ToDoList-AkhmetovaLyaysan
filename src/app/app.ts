8066143daa04dd26e2a8449f77991ca8776458dc
Liaisan829 <akhmetova1423@gmail.com>
2025-09-09 13:16:09 +0300

    homework-3

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { ToDoList } from '@components/to-do-list/to-do-list'
   selector: 'app-root',
   imports: [ToDoList],
   templateUrl: './app.html',
-  styleUrl: './app.sass'
+  styleUrl: './app.sass',
 })
 export class App {
   protected readonly title = signal('to-do-list-otus');
--- src/app/components/to-do-list-item/to-do-list-item.ts
+++ src/app/components/to-do-list-item/to-do-list-item.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, output} from '@angular/core';
 import {ToDo} from '../to-do-list/to-do-list';
+import {Button} from '@components/button/button';
 
 @Component({
   selector: 'app-to-do-list-item',
-  imports: [],
+  imports: [Button],
   templateUrl: './to-do-list-item.html',
   styleUrl: './to-do-list-item.sass',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToDoListItem {
   item = input.required<ToDo>();
